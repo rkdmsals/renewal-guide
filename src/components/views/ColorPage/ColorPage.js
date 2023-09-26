d42dfa49a3cee49084528273b549e2798e39d226
JEES23 <76464398+JEES23@users.noreply.github.com>
2023-09-26 10:44:41 +0900

    컬러 페이지 1차

---
+++ src/components/views/ColorPage/ColorPage.js
@@ -0,0 +1,38 @@
+import "./ColorPage.css";
+import React from 'react';
+
+function ColorPage() {
+
+    return (
+        <div className="ColorPage_Background">
+
+            <div className="ColorPage_inner">
+                <div className="ColorPage_script">
+                    <div className="ColorPage_title">
+                        새로워진 컬러
+                    </div>
+                    <div className="ColorPage_text">
+                        이화이언의 대표 색을 기존 사용하던 핑크에서 초록으로 변경하게 되었습니다. <br />
+                        먼저 바뀐 로고를 공개합니다! <br />
+                        새로운 컬러로 달라진 이화이언을 기대해주세요 
+                    </div>
+                </div>
+                <div className="ColorM_pictures">
+                    
+                </div>
+                <div className="ColorPage_pictures">
+                    
+                </div>
+            </div>
+            
+
+        </div>
+    )
+
+}
+
+
+export default ColorPage;
+
+
+
