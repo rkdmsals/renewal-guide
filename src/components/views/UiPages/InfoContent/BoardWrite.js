4ca0d0e47fe3587168692fdbbd6ecd1d23cf9810
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-09-28 12:37:21 +0900

    멘트 수정

--- src/components/views/UiPages/InfoContent/BoardWrite.js
+++ src/components/views/UiPages/InfoContent/BoardWrite.js
@@ -5,20 +5,20 @@ function BoardWrite() {
     <div className="ui_content_container">
       <div className="ui_content_wrap">
         <div className="ui_content_label">
-            새로워진 게시글 작성 방법으로 더 쉽게 원하는 글을 찾을 수 있어요! <br />
-            근무지역, 모집 기간 등의 상세정보를 간편하게 작성하고, 내가 원하는 알바,상품 등을 더욱 빠르게 찾아보세요.
+          새로워진 게시글 작성 방법으로 알바하자, 벼룩시장 등의 게시판에서 더 쉽게 원하는 글을 찾을 수 있게 되었습니다! <br />
+          근무지역, 모집 기간 등의 상세정보를 간편하게 작성하고, 내가 원하는 알바,상품 등을 더욱 빠르게 찾아보세요.
 
         </div>
       </div>
       <div className="img_wrap">
-      <div className="ui_image">
-        <div id="board_before" className="pic"><div className="img_text">알바하자</div></div>
-      </div>
-      <div className="ui_image">
-        <div id="board_after" className="pic"><div className="img_text">벼룩시장</div></div>
+        <div className="ui_image">
+          <div id="board_before" className="pic"><div className="img_text">알바하자</div></div>
+        </div>
+        <div className="ui_image">
+          <div id="board_after" className="pic"><div className="img_text">벼룩시장</div></div>
+        </div>
       </div>
     </div>
-  </div>
 
   );
 }
